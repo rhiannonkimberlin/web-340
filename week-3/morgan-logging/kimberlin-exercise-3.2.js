8b122ef165304e996525a9468d2bb70ade64fd59
rhiannonkimberlin <r.kim@live.com>
2020-08-22 13:47:01 -0700

    Adding Exercise 3.2

---
+++ week-3/morgan-logging/kimberlin-exercise-3.2.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var logger = require('morgan');
+
+var app = express();
+
+app.set('views', path.resolve(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+
+app.get('/', function(req, res){
+    res.render('index', {
+        message: 'Im excited to use the Morgan Logger!'
+    });
+});
+
+http.createServer(app).listen(3000, function() {
+    console.log('Application started and listening on port %s', 3000)
+})
