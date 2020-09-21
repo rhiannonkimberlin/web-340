96e9c4e1f3a5301d1b624e3380f0cf9d78ef9a07
rhiannonkimberlin <r.kim@live.com>
2020-09-21 08:38:01 -0700

    Adding 7.2

---
+++ week-7/tdd-example/test/kimberlin-exercise-7.2.js
@@ -0,0 +1,8 @@
+var assert = require("assert");
+
+describe("String#split", function(){
+    it("should return an array of fruits", function(){
+        assert(Array.isArray('Apple, Orange, Mango'.split(',')));
+    });
+});
+
