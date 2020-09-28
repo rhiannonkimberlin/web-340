0d53da03f46b0e8c1a46b07c8dab5588544cd4f4
rhiannonkimberlin <r.kim@live.com>
2020-09-28 10:17:46 -0700

    Adding 8.4

--- ems/app.js
+++ ems/app.js
@@ -2,8 +2,15 @@ var express = require("express");
 var http = require("http");
 var path = require("path");
 var logger = require("morgan");
+//added 9.21
+var helmet = require("helmet")
 var app = express();
 var mongoose = require('mongoose');
+//added 9.28
+var bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
+var csrf = require("csurf");
+var csrfProtection = csrf({cookie: true});
 
 //added 9.19
 var employee = require('./models/employee');
@@ -26,11 +33,33 @@ mongoose.connect(mongoDB, {
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(logger("short"));
+//added 9.21
+app.use(helmet.xssFilter());
 app.get("/", function (req, res) {
     res.render("index", {
-        title: 'Home Page'
+        title: 'Home Page',
+        //added message 9.21
+        message: "XSS Prevention Example"
     });
 });
+//added 9.28
+app.post("/process", function(req, res){
+    console.log(req.body.txtName);
+    res.redirect("/")
+});
+
+//added 9.28
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(cookieParser());
+app.use(csrfProtection);
+app.use(function(req, res, next) {
+    var token = request.csrfToken();
+    res.cookie('XSRF-TOKEN', token);
+    res.locals.csrfToken = token;
+    next();
+})
 
 //added 9.19 employee model
 var employee = new employee({
--- ems/models/employee.js
+++ ems/models/employee.js
@@ -3,7 +3,8 @@ var Schema = mongoose.Schema;
 
 let employeeSchema = new Schema({
     firstName: {type: String, required: true},
-    lastName: {type: String, required: true}
+    lastName: {type: String, required: true},
+    email: {type: String, required: true}
 });
 
 var employee = mongoose.model('employee', employeeSchema);
